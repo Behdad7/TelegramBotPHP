bcfe219174df81e058a9a74d1b3a327d681e2371
Mohammad Behdad <m.behdad7@gmail.com>
2016-11-08 23:08:54 +0330

    s

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ if(!is_null($text) && !is_null($chat_id)){
 			curl_close ($ch);
 
 		if (strpos( $server_output,"id_sina")){
-						$content = array('chat_id' => $chat_id, 'text' => "ایدی سینا" . $chat_id );
+						$content = array('chat_id' => $chat_id, 'text' => "ایدی سینا  " . $chat_id . $server_output );
 						$telegram->sendMessage($content);
 				}			
 				else{
