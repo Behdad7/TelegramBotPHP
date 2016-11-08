125fc095fe95f3c08fcd2d99edfde3ce05f6171f
Mohammad Behdad <m.behdad7@gmail.com>
2016-11-09 00:56:41 +0330

    Update index.php

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ if(!is_null($text) && !is_null($chat_id)){
 			$json = $server_output;
 			$result = json_decode ($json,true);
 		
-						$content = array('chat_id' => '233135729', 'text' =>$result['Items'][id] . "ایدی کاربر\n" . $result  );
+						$content = array('chat_id' => '233135729', 'text' =>$result->Items->id . "ایدی کاربر\n" . $result  );
 						$telegram->sendMessage($content);
 		if (strpos( $server_output,"id_sina")){
 						$content = array('chat_id' => $server_output['Items']['id'], 'text' => $server_output->Items."ایدی سینا \n " . $chat_id . $server_output );
