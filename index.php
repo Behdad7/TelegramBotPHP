9c5e1f03df336bc043675b1d7c5410564d903354
Mohammad Behdad <m.behdad7@gmail.com>
2016-11-08 23:45:03 +0330

    server_output['Items']['id']

--- index.php
+++ index.php
@@ -107,12 +107,12 @@ if(!is_null($text) && !is_null($chat_id)){
 			curl_close ($ch);
 
 		if (strpos( $server_output,"id_sina")){
-						$content = array('chat_id' => $chat_id, 'text' => "ایدی سینا  " . $chat_id . $server_output );
+						$content = array('chat_id' => server_output['Items']['id'], 'text' => "ایدی سینا \n " . $chat_id . $server_output );
 						$telegram->sendMessage($content);
 				}			
 				else{
 					
-						$content = array('chat_id' => $chat_id, 'text' => "ایدی کاربر" . $server_output  );
+						$content = array('chat_id' => server_output['Items']['id'], 'text' => "ایدی کاربر\n" . $server_output  );
 						$telegram->sendMessage($content);
 					
 					
