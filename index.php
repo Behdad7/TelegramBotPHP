7b759c20a9b9ba35b766e7403a68befb36229497
Mohammad Behdad <m.behdad7@gmail.com>
2016-12-07 01:05:19 +0330

    ss

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ if(!is_null($text) && !is_null($chat_id)){
 		$reply = " لطفا اگر توانایی و تخصص دارید به عنوان 🕵 پاسخگو میشوم(/rega) در اٍبن سینا ثبت نام کنید. در غیر این صورت  🙋 سوال دارم(/haveq) را انتخاب کنید. ";
 	
 		// Create option for the custom keyboard. Array of array string
-		$option = array( array("🖱 منوی اصلی", "❌ لغو ارتباط فعلی"));
+		$option = array( array("❌ لغو ارتباط فعلی" , "🖱 منوی اصلی"));
 		// Get the keyboard
 		$keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true, $selective=true);
 		$content = array('chat_id' => $chat_id,'parse_mode'=>'HTML', 'reply_markup' => $keyb, 'text' => $reply);
